1908b7bb5f032ab546400508267d2feea71c95d2
Marcelo Cordeiro Basílio <marcelocbasilio@gmail.com>
2026-03-28 17:10:37 -0300

    It was moved to show the slug.

--- app/zero/[name]/page.tsx
+++ app/zero/[name]/page.tsx
@@ -1,11 +1,13 @@
 import MyName from "@/app/components/MyName";
 import Hobbies from "@/app/components/Hobbies";
+import {Img} from "@/app/components/Img";
 
 export default function Page() {
   return (
       <div className="page">
         <MyName name="Marcelo" age={46} birthday={new Date(1980, 8, 17)}/>
         <Hobbies/>
+        <Img/>
       </div>
   )
 }
