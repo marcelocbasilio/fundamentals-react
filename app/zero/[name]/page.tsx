7a65381a3f0f6de5ffe39f1230fa8ca4cc7bbf5c
Marcelo Cordeiro Basílio <marcelocbasilio@gmail.com>
2026-03-28 17:14:58 -0300

    It was modified to learn about slugs.

--- app/zero/[name]/page.tsx
+++ app/zero/[name]/page.tsx
@@ -1,13 +1,25 @@
 import MyName from "@/app/components/MyName";
 import Hobbies from "@/app/components/Hobbies";
 import {Img} from "@/app/components/Img";
+import Link from "next/link";
 
-export default function Page() {
+type PageProps = {
+  params: Promise<{ name: string }>
+}
+
+const Page = async ({params}: PageProps) => {
+  const {name} = await params;
   return (
-      <div className="page">
-        <MyName name="Marcelo" age={46} birthday={new Date(1980, 8, 17)}/>
+      <div className="grid gap-y-4">
+        <MyName name={name} age={46} birthday={new Date(1980, 8, 17)}/>
         <Hobbies/>
-        <Img/>
+        <div>
+          <p>I like:</p>
+          <Img/>
+        </div>
+        <Link className="underline" href="/zero">Back</Link>
       </div>
   )
 }
+
+export default Page
