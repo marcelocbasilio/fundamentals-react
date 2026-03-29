fb191761af236f9e18e3cdb94847ecae61fe11f1
Marcelo Cordeiro Basílio <marcelocbasilio@gmail.com>
2026-03-29 14:33:16 -0300

    The Counter page has been modified to include the Btn component for practice and study.

--- app/components/Counter.tsx
+++ app/components/Counter.tsx
@@ -1,14 +1,58 @@
 "use client"
 
 import {useState} from "react";
+import Btn from "@/app/components/Btn";
 
 const Counter = () => {
-  const [counter, _setCounter] = useState(0)
+  const [counter, setCounter] = useState(0)
+  const [thing, setThing] = useState("")
 
   return (
-      <div>
+      <div className="grid gap-y-4">
         <h2 className="text-2xl">Counter</h2>
         <p>Current number: {counter}</p>
+        <div className="flex gap-x-2">
+          <Btn
+              onClick={() => {
+                setCounter((count) => count - 3)
+              }}
+          >-3
+          </Btn>
+
+          <Btn
+              onClick={() => {
+                setCounter((count) => count - 1)
+              }}
+          >-1
+          </Btn>
+          <Btn
+              onClick={() => {
+                setCounter((count) => count + 1)
+              }}
+          >+1
+          </Btn>
+
+          <Btn
+              onClick={() => {
+                setCounter((count) => count + 3)
+              }}
+          >+3
+          </Btn>
+        </div>
+
+        <div className="flex gap-x-2">
+          <input
+              className="px-4 py-1 border border-gray-500"
+              value={thing}
+              onChange={(e) => setThing(e.target.value)}
+          />
+          <Btn
+              onClick={() => {
+                setThing("")
+              }}
+          >Clear
+          </Btn>
+        </div>
       </div>
   )
 }
